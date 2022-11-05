6fa89d39d28f3bde5c7242a176ad6dd5df173f4c
简 <2447746158@qq.com>
2022-11-05 15:47:44 +0800

    finish rawMode

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,56 @@
 const std = @import("std");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+const C = @cImport({
+    @cInclude("ctype.h");
+    @cInclude("termios.h");
+    @cInclude("unistd.h");
+});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+const RawModeError = error {
+    NotATerminal,
+};
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+fn enableRawMode() RawModeError!C.termios {
+    var orig: C.termios = undefined;
 
-    try bw.flush(); // don't forget to flush!
+    if (C.tcgetattr(C.STDIN_FILENO, &orig) == -1) {
+        return error.NotATerminal;
+    }
+
+    var raw = orig;
+    raw.c_cflag |= C.CS8;
+    raw.c_iflag &= ~@as(c_ulong, C.ICRNL | C.IXON);
+    raw.c_lflag &= ~@as(c_ulong, C.ECHO | C.ICANON | C.IEXTEN | C.ISIG);
+    raw.c_oflag &= ~@as(c_ulong, C.OPOST);
+    raw.c_cc[C.VMIN] = 0;
+    raw.c_cc[C.VTIME] = 1;
+
+    _ = C.tcsetattr(C.STDIN_FILENO, C.TCSAFLUSH, &raw);
+
+    return orig;
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn disableRawMode(orig: C.termios) void {
+    _ = C.tcsetattr(C.STDIN_FILENO, C.TCSAFLUSH, &orig);
+}
+
+pub fn main() void {
+    if (enableRawMode()) |orig| {
+        defer disableRawMode(orig);
+    } else |err| {
+        std.debug.print("Failed to enable raw mode: {}\n", .{err});
+        return;
+    }
+
+    while (true) {
+        var c: u8 = 0;
+        _ = C.read(C.STDIN_FILENO, &c, 1);
+        if (C.iscntrl(c) != 0) {
+            std.debug.print("{}\r\n", .{c});
+        } else {
+            std.debug.print("{}: {c}\r\n", .{ c, c });
+        }
+
+        if (c == 'q') break;
+    }
 }
