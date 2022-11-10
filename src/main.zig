4360b4ae0eef17e88599be1add00f4d48ca20753
简 <2447746158@qq.com>
2022-11-10 15:36:57 +0800

    improve error message printing

--- src/main.zig
+++ src/main.zig
@@ -1,23 +1,38 @@
 const std = @import("std");
+const utils = @import("utils.zig");
 
 const editor = @import("editor.zig");
 const raw = @import("rawMode.zig");
 
-pub fn main() void {
+pub fn main() !void {
     const allocator = std.heap.page_allocator;
+
+    var errMessageBuf = std.ArrayList(u8).init(allocator);
+    defer {
+        std.debug.print("{s}\n", .{errMessageBuf.items});
+        errMessageBuf.clearAndFree();
+    }
+
     const args = std.process.argsAlloc(allocator) catch |err| {
-        std.debug.panic("Fail to get args: {}", .{err});
+        utils.bufAppendWithFmt(400, &errMessageBuf,
+            "Fail to get args: {}\n", .{err}
+        ) catch return;
         return;
     };
     defer std.process.argsFree(allocator, args);
 
     const orig = raw.enableRawMode() catch |err| {
-        std.debug.print("Failed to enable raw mode: {}\n", .{err});
+        utils.bufAppendWithFmt(400, &errMessageBuf,
+            "Failed to enable raw mode: {}\n", .{err}
+        ) catch return;
         return;
     };
     defer raw.disableRawMode(orig);
 
     editor.editorProgress(args) catch |err| {
-        std.debug.print("Failed when running editor: {}\n", .{err});
+        utils.bufAppendWithFmt(400, &errMessageBuf,
+            "Failed when running editor: {}\n", .{err}
+        ) catch return;
+        return;
     };
 }
